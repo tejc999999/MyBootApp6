27bd9c51a9aaabfe1f1e4b770dce192959171e5c
tejc999999 <tejc999999@gmail.com>
2018-05-20 21:06:58 +0900

    Appクラスパス変更（バグ修正）

--- src/main/java/jp/te4a/spring/boot/myapp13/BookApplication.java
+++ src/main/java/jp/te4a/spring/boot/myapp13/BookApplication.java
@@ -1,4 +1,4 @@
-package jp.te4a.spring.boot.myapp13.app;
+package jp.te4a.spring.boot.myapp13;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
